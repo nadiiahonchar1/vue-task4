7ae360040b3ac00a872205b6eda0af7952397211
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2024-01-02 22:57:55 +0200

    fixed

--- src/components/AppButton.vue
+++ src/components/AppButton.vue
@@ -19,4 +19,30 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.btn {
+  display: inline-block;
+  min-width: 100px;
+  height: 30px;
+  border-radius: 10px;
+  background-color: #2756a8ad;
+  border: transparent;
+  color: white;
+  cursor: pointer;
+}
+
+.danger {
+  color: rgb(219, 29, 29);
+  border: 1px solid red;
+}
+
+.primary {
+  color: orange;
+  border: 1px solid orange;
+}
+
+.btn:hover {
+  border: transparent;
+  background-color: rgb(0, 119, 128);
+}
+</style>
