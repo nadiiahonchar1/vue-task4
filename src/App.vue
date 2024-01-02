1739e268fffd59b12aa2ecbc4495c56e408be819
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2024-01-02 15:44:11 +0200

    added AppNews

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,34 @@
     <div class="card-title">
       <h2>Current news {{ now }}</h2>
       <hr />
+      <span>Open:{{ openRate }}</span>
     </div>
-    <div class="card" v-for="item in news" :key="item">
-      <h3>{{ item }}</h3>
-      <button class="btn">Open</button>
-    </div>
+    <app-news
+      v-for="item in news"
+      :key="item.id"
+      :title="item.title"
+      :id="item.id"
+      :is-open="item.isOpen"
+    ></app-news>
   </div>
 </template>
 
 <script>
+import AppNews from "./components/AppNews.vue";
 export default {
   name: "App",
   data() {
     return {
       now: new Date().toLocaleDateString(),
-      news: ["News1", "News2"],
+      openRate: 0,
+      news: [
+        { title: "News1", id: 1, isOpen: false },
+        { title: "News2", id: 2, isOpen: false },
+      ],
     };
+  },
+  components: {
+    "app-news": AppNews,
   },
 };
 </script>
---
+++ src/components/AppNews.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="card">
+    <h3>{{ title }}</h3>
+    <button class="btn" @click="open">Open</button>
+    <p v-if="isNewOpen">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, iusto in
+      voluptatibus, esse libero pariatur ab delectus impedit dicta deleniti
+      error totam, mollitia expedita inventore temporibus quidem fuga omnis sunt
+      hic. Quas recusandae ex quod repellat, veniam sit. Cumque repellat nam non
+      provident aut quae nisi eum, optio quia commodi enim nobis ratione
+      voluptas placeat aspernatur tempore magnam debitis necessitatibus omnis
+      doloremque nesciunt earum pariatur velit asperiores! Nemo obcaecati
+      deleniti officia corporis eos, labore vero ipsa laboriosam iusto dolorum
+      id at reiciendis sapiente distinctio unde, optio ratione error itaque? At
+      alias exercitationem accusamus veritatis quod, blanditiis molestias
+      adipisci necessitatibus, quos, sed assumenda vitae eum possimus eveniet
+      deleniti voluptatem. Praesentium unde odio autem aperiam deserunt tempora
+      consectetur perspiciatis quaerat. Qui reprehenderit assumenda fuga esse
+      nihil voluptatum, ducimus reiciendis voluptates quod eligendi deleniti
+      velit porro aliquid doloribus consectetur itaque harum in at. Id
+      voluptatem illo fuga quam dolores sapiente qui ullam debitis dolorem
+      mollitia esse explicabo molestiae, et modi architecto culpa error aliquid?
+      Autem reprehenderit debitis nam eum laboriosam aperiam facilis quod
+      similique eius maiores dolor omnis nobis labore doloribus nesciunt
+      accusantium maxime incidunt hic, voluptatibus delectus perspiciatis sunt?
+      Dolor quae qui molestias totam nesciunt. Est sit tenetur excepturi,
+      temporibus quisquam commodi?
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: { type: String, required: true },
+    id: { type: Number, required: true },
+    isOpen: { type: Boolean, required: false, default: false },
+  },
+  data() {
+    return {
+      isNewOpen: this.isOpen,
+    };
+  },
+  methods: {
+    open() {
+      this.isNewOpen = !this.isNewOpen;
+    },
+  },
+};
+</script>
